68e978200a3746bef14cebe5585d74c78fe30090
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2025-01-09 20:11:44 -0500

    Add routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import Login from "./pages/Login.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   </BrowserRouter>,
 );
