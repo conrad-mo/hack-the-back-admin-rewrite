b61fbd2de162f582b250b9ce22beb68aa8a479c8
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2025-01-09 20:38:16 -0500

    Dummy piechart + messing with theme

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,5 +1,11 @@
+import { Piechart } from "../components/elements/Piechart";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Piechart />
+    </>
+  );
 }
 
 export default App;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 function Login() {
-  return <></>;
+  return (
+    <>
+      <Button>Click me</Button>
+    </>
+  );
 }
 
 export default Login;
