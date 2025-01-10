573a7c364ea84d1d8f75ea97118968d7c66e6c84
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2025-01-10 15:47:24 -0500

    Improve nav menu + layout some groundwork for applicant page

--- src/components/Navmenu.tsx
+++ src/components/Navmenu.tsx
@@ -1,27 +1,34 @@
 import { useContext } from "react";
 import { UserContext } from "@/utils/auth";
-import { useNavigate } from "react-router";
 import { Button } from "./ui/button";
+import { House, Newspaper } from "lucide-react";
+import { Link } from "react-router";
 
 function NavMenu() {
   const { logout } = useContext(UserContext) ?? {};
-  const navigate = useNavigate();
-  const handleLogout = async () => {
-    if (logout) {
-      logout();
-      navigate("/login");
-    }
-  };
 
   return (
-    <div className="flex flex-col min-w-16 w-1/5 max-w-1/6 border gap-4 justify-between p-4">
-      <div className="flex flex-col gap-4">
-        <h1 className="font-semibold text-lg">Hack The Back</h1>
-        <Button variant="ghost" className="bg-accent text-accent-foreground">
-          Home
+    <div className="flex flex-col min-w-16 w-1/5 max-w-[16rem] border gap-4 justify-between p-4">
+      <div className="flex flex-col gap-2">
+        <h1 className="font-semibold text-md p-2">Hack The Back</h1>
+        <Button
+          variant="secondary"
+          asChild
+          className="inline-flex justify-start"
+        >
+          <Link to="/">
+            <House />
+            Home
+          </Link>
+        </Button>
+        <Button variant="ghost" asChild className="inline-flex justify-start">
+          <Link to="/apps">
+            <Newspaper />
+            Hacker Apps
+          </Link>
         </Button>
       </div>
-      <Button onClick={handleLogout} variant="secondary">
+      <Button onClick={logout} variant="secondary">
         Logout
       </Button>
     </div>
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import Home from "./pages/Home.tsx";
 import Login from "./pages/Login.tsx";
 import { AuthProvider } from "./utils/auth.tsx";
+import Apps from "./pages/Apps.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
@@ -11,6 +12,7 @@ createRoot(document.getElementById("root")!).render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/apps" element={<Apps />} />
       </Routes>
     </AuthProvider>
   </BrowserRouter>,
