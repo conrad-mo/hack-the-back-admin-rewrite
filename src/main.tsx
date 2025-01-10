dcc7b3101e73eceed75fbf1de4d85f834713f839
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2025-01-09 20:18:42 -0500

    Add tailwindcss

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import App from "./pages/App.tsx";
 import Login from "./pages/Login.tsx";
 
 createRoot(document.getElementById("root")!).render(
